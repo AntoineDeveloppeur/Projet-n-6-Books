f6ee6e6ad6ffbd2d14690734b12a6859554a3ea5
Antoine V <antoine.verove@gmail.com>
2024-06-19 14:18:35 +0200

    Toujours en phase de deboggage pour la fonction signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,22 +1,48 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
+// exports.signup = (req, res, next) => {
+//     console.log('je suis dans signup', req.body.password)
+//     const objet = { objet: 'objet' }
+
+//     objet
+//         .save()
+//         .then(() => res.status(201).json({ message: 'utilisateur enregistré' }))
+//         .catch((error) => {
+//             console.log('error dans le save')
+//             res.status(408).json({ message: error })
+//         })
+
+//         .catch((error) => {
+//             console.log('erreur dans le catch de hash')
+//             res.status(503).json({ error })
+//         })
+// }
+
 exports.signup = (req, res, next) => {
     console.log('je suis dans signup', req.body.password)
     bcrypt
-        .hash(req.body.password, 10)
+        .hash(req.body.password, 1)
         .then((hash) => {
-            const user = new UserSchema({
-                email: req.email,
+            console.log(hash)
+            const user = new User({
+                email: req.body.email,
                 password: hash,
             })
+            console.log(user)
             user.save()
                 .then(() =>
                     res.status(201).json({ message: 'utilisateur enregistré' })
                 )
-                .catch((error) => res.status(400).json({ message: error }))
+                .catch((error) => {
+                    console.log('error dans le save')
+                    res.status(408).json({ error })
+                })
         })
-        .catch((error) => res.status(500).json({ error }))
+        .catch((error) => {
+            console.log('erreur dans le catch de hash')
+            res.status(503).json({ error })
+        })
 }
 
 exports.login = (req, res, next) => {}
