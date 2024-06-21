706f29d0930915e9d7bdb2492e7d42f783e87987
Antoine V <antoine.verove@gmail.com>
2024-06-21 13:41:10 +0200

    Problème de connection à la base de donnée résolu. J'ai utilisé mongoose pour connecter à la base de donné et cela fonctionne depuis

--- backend/app.js
+++ backend/app.js
@@ -4,68 +4,70 @@ const { MongoClient, ServerApiVersion } = require('mongodb')
 const user = require('./routes/User')
 const userRoutes = require('./routes/User')
 const bookRoutes = require('./routes/Book')
+const mongoose = require('mongoose')
 
-// CORS configuration
-const corsOptions = {
-    origin: 'http://localhost:3000', // Adjust this to match your frontend's origin
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-}
 const app = express()
 
-app.use(cors(corsOptions))
+// // CORS configuration
+// const corsOptions = {
+//     origin: '*', // Adjust this to match your frontend's origin
+//     methods: 'GET,POST,PUT,DELETE,OPTIONS',
+//     allowedHeaders: 'Content-Type, Authorization',
+// }
 
-// Handle preflight OPTIONS request
-app.options('*', cors(corsOptions))
+// app.use(cors(corsOptions))
+
+// // Handle preflight OPTIONS request
+// app.options('*', cors(corsOptions))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
+    next()
+})
 
 app.use(express.json())
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-//     )
-//     res.setHeader(
-//         'Access-Control-Allow-Methods',
-//         'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-//     )
-//     next()
+mongoose
+    .connect(
+        'mongodb+srv://antoineverove:TPKIpIN61zJvWmp5@antoineserver.fndalaw.mongodb.net/?retryWrites=true&w=majority&appName=Antoineserver',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+// const uri =
+//     'mongodb+srv://antoineverove:TPKIpIN61zJvWmp5@antoineserver.fndalaw.mongodb.net/?retryWrites=true&w=majority&appName=Antoineserver'
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     },
 // })
-
-// mongoose
-//     .connect(
-//         'mongodb+srv://antoineverove:TPKIpIN61zJvWmp5@antoineserver.fndalaw.mongodb.net/?retryWrites=true&w=majority&appName=Antoineserver',
-//         { useNewUrlParser: true, useUnifiedTopology: true }
-//     )
-//     .then(() => console.log('Connexion à MongoDB réussie !'))
-//     .catch(() => console.log('Connexion à MongoDB échouée !'))
-
-const uri =
-    'mongodb+srv://antoineverove:TPKIpIN61zJvWmp5@antoineserver.fndalaw.mongodb.net/?retryWrites=true&w=majority&appName=Antoineserver'
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-})
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect()
-        // Send a ping to confirm a successful connection
-        await client.db('admin').command({ ping: 1 })
-        console.log(
-            'Pinged your deployment. You successfully connected to MongoDB!'
-        )
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close()
-    }
-}
-run().catch(console.dir)
+// async function run() {
+//     try {
+//         // Connect the client to the server	(optional starting in v4.7)
+//         await client.connect()
+//         // Send a ping to confirm a successful connection
+//         await client.db('admin').command({ ping: 1 })
+//         console.log(
+//             'Pinged your deployment. You successfully connected to MongoDB!'
+//         )
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close()
+//     }
+// }
+// run().catch(console.dir)
 
 module.exports = app
 
--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,6 +1,7 @@
 const Books = require('../models/book')
 
 exports.getAllBooks = (req, res, next) => {
+    console.log('lapplication reçoit la consigne de donner tous les bouquins')
     Books.find()
         .then((allBooks) => {
             console.log('voici tous les books', allBooks)
@@ -9,11 +10,27 @@ exports.getAllBooks = (req, res, next) => {
         .catch((error) => res.status(400).json({ error }))
 }
 
-exports.getABook = (req, res, next) => {}
+exports.getABook = (req, res, next) => {
+    Books.findOne({ _id: req.params.id })
+        .then((book) => {
+            res.status(200).json(book)
+        })
+        .catch((error) => res.status(400).json({ error }))
+}
+
+exports.postABook = (req, res, next) => {
+    const newBook = new Books({
+        userId: req.body.userId,
+        title: req.body.title,
+        author: req.body.author,
+        imageUrl: req.body.imageUrl,
+        year: req.body.year,
+        ratings: [], // La notation est laissé vide, à voir comment la base de donnée accepte les propriétés vide
+        averageRating: null, // - Est-ce que null convient pour ne rien mettre pour un chiffre ?
+    })
+}
+
 exports.getBestRatings = (req, res, next) => {}
-exports.postABook = (req, res, next) => {}
 exports.modifyABook = (req, res, next) => {}
 exports.deleteABook = (req, res, next) => {}
 exports.rateABook = (req, res, next) => {}
-
-module.exports = Books
